87018e0eaf5732ce4272ec57a29fe839b69d497b
Matheus Bloize <mbloizepd@gmail.com>
2025-09-26 18:47:04 -0300

    feat: setup finished

---
+++ Domain/DTOs/LoginDTO.cs
@@ -0,0 +1,6 @@
+namespace dio_net_minimals_api.Domain.DTOs;
+
+public class LoginDTO {
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
---
+++ Infra/DB/DBContext.cs
@@ -0,0 +1,5 @@
+namespace dio_net_minimals_api.Infra.DB;
+
+public class DBContext {
+
+}
